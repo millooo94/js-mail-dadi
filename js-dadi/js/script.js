6b3e9e2336c8c6b7fffa2250f7ca90a64ce50a36
millooo94 <camillomusmeciunict@gmail.com>
2022-10-26 01:09:50 +0200

    Refactor all

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -1,14 +1,38 @@
-const userNumber = Math.floor(Math.random() * 6)
-console.log('TUO NUMERO ', userNumber)
-const PcNumber = Math.floor(Math.random() * 6)
-console.log('NUMERO PC ', PcNumber)
+const user = document.querySelector('.your-number');
+const pc = document.querySelector('.pc-number')
+const result = document.querySelector('.result');
 
-if (userNumber > PcNumber) {
-    console.log('HAI VINTO')
-} else if (userNumber == PcNumber) {
-    console.log('HAI PAREGGIATO')
-} else {
-    console.log('HAI PERSO')
-}
-    
+
+result.addEventListener("click", function game(){
+    const userNumber = Math.floor(Math.random() * 6)
+    const PcNumber = Math.floor(Math.random() * 6)
+    user.innerHTML = userNumber
+    pc.innerHTML = PcNumber
+
+
+    if (userNumber > PcNumber) {
+        result.innerHTML = 'YOU WIN'
+        result.removeAttribute('class')
+        result.classList.add('result')
+        result.classList.add('my_text2')
+        result.classList.add('positive')
+    } else if (userNumber == PcNumber) {
+        result.innerHTML = 'DRAW'
+        result.removeAttribute('class')
+        result.classList.add('result')
+        result.classList.add('my_text2')
+        result.classList.add('draw')
+    } else {
+        result.innerHTML = 'YOU LOST'
+        result.removeAttribute('class')
+        result.classList.add('result')
+        result.classList.add('my_text2')
+        result.classList.add('negative')
+    }
+    })
+
+
+
+
+
  
