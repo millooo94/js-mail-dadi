0265e34ea0f9aaeaabe9d55d0fd71f32044a5f5b
millooo94 <camillomusmeciunict@gmail.com>
2022-10-25 15:31:07 +0200

    Add button

---
+++ js-mail/js/script.js
@@ -0,0 +1,64 @@
+// const eleUserMail = document.querySelector('.user-mail');
+// const eleListMail = [
+//     'intlprog@yahoo.com',
+//     'szymansk@aol.com',
+//     'richard@sbcglobal.net',
+//     'pappp@comcast.net',
+//     'horrocks@verizon.net',
+//     'kempsonc@sbcglobal.net',
+//     'lamky@comcast.net',
+//     'sokol@me.com',
+//     'cisugrad@verizon.net',
+//     'eabrown@hotmail.com',
+//     'wiseb@comcast.net',
+//     'zeitlin@aol.com'
+// ]
+
+// const result = document.getElementById('block')
+// const generate = document.querySelector('.btn')
+
+// generate.addEventListener("click", function mail(){
+
+//     for (let i = 0; i < eleListMail.length; i++) {
+
+//         if (eleUserMail.value === eleListMail[i]) {
+            
+//             result.innerHTML = 'EMAIL CORRETTA'
+//         } else {
+//             result.innerHTML = 'EMAIL SBAGLIATA' 
+//         }
+//     }
+// })
+
+
+
+
+
+const eleUserMail = prompt('mail');
+const eleListMail = [
+    'intlprog@yahoo.com',
+    'szymansk@aol.com',
+    'richard@sbcglobal.net',
+    'pappp@comcast.net',
+    'horrocks@verizon.net',
+    'kempsonc@sbcglobal.net',
+    'lamky@comcast.net',
+    'sokol@me.com',
+    'cisugrad@verizon.net',
+    'eabrown@hotmail.com',
+    'wiseb@comcast.net',
+    'zeitlin@aol.com'
+]
+
+const result = document.getElementById('block')
+
+for (let i = 0; i < eleListMail.length; i++) {
+
+    if (eleUserMail === eleListMail[i]) {
+        console.log('EMAIL CORRETTA')
+        result.innerHTML = 'EMAIL CORRETTA'
+    } else {
+        result.innerHTML = 'EMAIL SBAGLIATA'
+        console.log('EMAIL SBAGLIATA')
+    }
+}
